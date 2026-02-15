89c2085b813e29b0fd5c473086374651a88af0fb
nazmusweb.coding <nazmusweb.coding@gmail.com>
2026-02-15 18:06:32 +0600

    Added Topic: Comment

--- JavaScript/Shradha Khapra/Operators and Conditional Statements/script.js
+++ JavaScript/Shradha Khapra/Operators and Conditional Statements/script.js
@@ -1,1 +1,12 @@
+// Comment
+// Single line comment
+// This code prints Hello World!
 console.log("Hello World!")
+
+
+// Multi-line comment
+/**
+ * This code that prints hello world
+ * on our console window
+ */ 
+console.log("hello woeld");
