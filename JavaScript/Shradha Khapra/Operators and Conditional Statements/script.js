8968f9fea64aede9e111c65b12d5d0eddfecd447
nazmusweb.coding <nazmusweb.coding@gmail.com>
2026-02-17 21:30:40 +0600

    Add switch statement

--- JavaScript/Shradha Khapra/Operators and Conditional Statements/script.js
+++ JavaScript/Shradha Khapra/Operators and Conditional Statements/script.js
@@ -206,3 +206,17 @@ if (age >= 0 && age <= 12) {
  */
 let result = age >= 18 ? "adult" : "not adult";
 console.log(result);
+
+// Switch Statement
+const fruit = "Papayas";
+switch (fruit) {
+    case "Oranges":
+        console.log("Oranges are $0.59 a pound.");
+        break;
+    case "Mangoes":
+    case "Papayas":
+        console.log("Mangoes and papayas are $2.79 a pound.");
+        break;
+    default:
+        console.log(`Sorry, we are out of ${fruit}.`);
+}
