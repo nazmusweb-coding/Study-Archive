99daea6783a6488055d506a53e32397fa7878bc4
nazmusweb.coding <nazmusweb.coding@gmail.com>
2026-02-17 21:55:36 +0600

    Create new folder for loops and strings

---
+++ JavaScript/Shradha Khapra/Loops and Strings/script.js
@@ -0,0 +1,9 @@
+/**
+ * Loops in JS
+ * Loops are used to execute a piece of code again and again
+ * for Loop
+ * while Loop
+ * do-while Loop
+ * for-of Loop (special loop)
+ * for-in Loop (special loop)
+ */
