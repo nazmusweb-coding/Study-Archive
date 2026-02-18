95e89677a6e66bc9c84f0ed72ff98e5a5391367d
nazmusweb.coding <nazmusweb.coding@gmail.com>
2026-02-18 21:52:36 +0600

    Add infinite loop concept

--- JavaScript/Shradha Khapra/Loops and Strings/script.js
+++ JavaScript/Shradha Khapra/Loops and Strings/script.js
@@ -20,3 +20,13 @@ for (let i = 1; i <= 5; i++) {
 
 console.log("for loop has ended");
 
+/**
+ * Infinite loop: a loop that never ends
+ * stopping condition is always true
+ * 
+ * for (let i = 1; i >= 0; i++) {
+ *      console.log(i =, i);
+ * }
+ * 
+ * it is a infinite loop
+ */
