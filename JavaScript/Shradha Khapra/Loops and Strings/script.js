11340d2b50202328aee36614243736288a3bae51
nazmusweb.coding <nazmusweb.coding@gmail.com>
2026-02-18 23:58:41 +0600

    Add do-while loop

--- JavaScript/Shradha Khapra/Loops and Strings/script.js
+++ JavaScript/Shradha Khapra/Loops and Strings/script.js
@@ -42,3 +42,19 @@ while (i <= 5) {
     console.log("apna college");
     i++;
 }
+
+console.log("while loop has ended");
+
+/**
+ * do-while Loop
+ * condition is checked at last
+ * so at least 1 iteration will
+ * take place even if the condition is false
+ */
+let j = 1;
+do {
+    console.log("apna college");
+    j++;
+} while (j <= 5);
+
+console.log("do-while loop has ended");
