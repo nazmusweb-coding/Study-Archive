8b0037da60cf5b3cfb2e4f54a952ae70704fe789
nazmusweb.coding <nazmusweb.coding@gmail.com>
2026-02-19 22:23:53 +0600

    Add question problem 2

--- JavaScript/Shradha Khapra/Loops and Strings/Practice/script.js
+++ JavaScript/Shradha Khapra/Loops and Strings/Practice/script.js
@@ -5,3 +5,18 @@ for (let i = 0; i <= 100; i++) {
         console.log(`${i} is even number.`);
     }
 }
+
+/**
+ * Qs2. Create a game where you start with any random
+ * game number. Ask the user to keep guessing the game
+ * number until the user enters correct value.
+ */
+
+let gameNumber = 25;
+let userNumber = prompt("Guess the game number:");
+
+while (gameNumber != userNumber) {
+    userNumber = prompt("You entered wrong number. Guess again:");
+}
+
+console.log("You guessed it correctly.");
