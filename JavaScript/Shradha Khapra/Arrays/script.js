f9d7f173d7f39848b5de8cc07bf594eff1098edd
nazmusweb.coding <nazmusweb.coding@gmail.com>
2026-02-21 22:29:11 +0600

    Add Arrays Topic

---
+++ JavaScript/Shradha Khapra/Arrays/script.js
@@ -0,0 +1,10 @@
+/**
+ * Arrays
+ * Collections of items
+ */
+
+// Create Array
+let heroes = ["ironman", "hulk", "thor", "batman"];
+let marks = [96, 75, 48, 83, 66];
+let info = ["rahul", 86, "Delhi"];
+
