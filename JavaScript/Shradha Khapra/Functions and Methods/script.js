ea9210632b79f8aa406988bd3ebc610c326cbd13
nazmusweb.coding <nazmusweb.coding@gmail.com>
2026-02-28 22:07:55 +0600

    Add function definition with param/without param, function call with argu and without argu

--- JavaScript/Shradha Khapra/Functions and Methods/script.js
+++ JavaScript/Shradha Khapra/Functions and Methods/script.js
@@ -3,3 +3,35 @@
  * Block of code that performs a specific task,
  * can be invoked whenever needed
  */
+
+/**
+ * Function Definition
+ * 
+ * function functionName() {
+ *     // do some work
+ * }
+ * 
+ * function functionName(param1, param2, ...) {
+ *     // do some work
+ * }
+ */
+
+/**
+ * Function Call / Invoke
+ * 
+ * functionName();
+ */
+
+function myFunction() {
+    console.log("Welcome to Apna College");
+    console.log("We are learning JavaScript");
+}
+
+function printMe(message) {
+    console.log(message);
+}
+
+function sum(x, y) {
+    s = x + y;
+    return s;
+}
